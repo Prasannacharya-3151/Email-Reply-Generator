c627a00fe0e330e301be8b3b1c53cd2470343e44
Prasannacharya-3151 <prasannacharya428@gmail.com>
2025-09-01 19:54:40 +0530

    completed

--- src/components/EmailReplyGenerator.tsx
+++ src/components/EmailReplyGenerator.tsx
@@ -10,8 +10,8 @@ const EmailReplyGenerator: React.FC = () => {
             alert("Please enter email content");
             return;
         }
-
-        const generator = `Hey John, \n\nthis is a {$tone.toLowerCase()} replay to your Email:\n"${emailContent}"\n\nLooking forward to hearing from you! `;
+ const generator = `Hey, Hi I am Prasann Acharya What Can I help u bruh!!....`;
+       
         setReplay(generator);
     }
 
@@ -23,7 +23,11 @@ const handleCopy = ()=>{
 }
 
   return (
-    <div className="max-w-2xl mx-auto p-6 bg-white shadow-lg rounded-2xl space-y-5">
+    <div
+  className="min-h-screen py-40 items-center justify-center bg-cover bg-center "
+  style={{ backgroundImage: "url('https://images.pexels.com/photos/4050334/pexels-photo-4050334.jpeg?_gl=1*1tkorog*_ga*NzA2MjIwNDMwLjE3NDc3MTI4NTI.*_ga_8JE65Q40S6*czE3NTY3MzU5NzckbzYkZzEkdDE3NTY3MzYwODckajM3JGwwJGgw')" }}
+>
+    <div className="max-w-2xl mx-auto p-6  shadow-lg rounded-2xl space-y-5">
         <h1 className="text-3xl font-bold mb-4">Email Reply Generator</h1>
         <div>
             <label className="block mb-2 font-medium">Original Email Content</label>
@@ -41,9 +45,9 @@ const handleCopy = ()=>{
             onChange={(e) => setTone(e.target.value)}
             >
                 <option value="Casual">Casual</option>
-                <option value="Formal">Casual</option>
-                <option value="Friendly">Casual</option>
-                <option value="Proffesion">Casual</option>
+                <option value="Formal">Formal</option>
+                <option value="Friendly">Friendly</option>
+                <option value="Proffesion">Proffesion</option>
             </select>
         </div>
         <button 
@@ -55,17 +59,25 @@ const handleCopy = ()=>{
         </button>
 
         {replay && (
-            <div className="border rounded-lg p-4 bg-gray-50">
+            <div className="border rounded-lg p-4">
                 <h2 className="font-semibold mb-2">Generated Reply</h2>
                 <textarea
                 readOnly
-                className="w-full border ronded-lg p-3 h-32 bg-gray-100"
+                className="w-full border ronded-lg p-3 h-32 focus:ring focus:outline-none"
                 value={replay}
                 />
 
                 <button
-        )}
+                onClick={handleCopy}
+                className="mt-3 px-4 py-2 bg-gray-800 text-white rounded-lg hover:bg-gray-900"
+                >
+                    Copy to Clipboard
+                </button>
+       
     </div>
+  )}
+  </div>
+</div>
   );
 };
 
